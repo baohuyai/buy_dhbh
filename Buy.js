51924f9446de35fcb87541997d9535a63a88d984
baohuyai <151175691+baohuyai@users.noreply.github.com>
2024-02-09 15:49:44 +0700

    Update Buy.js

--- Buy.js
+++ Buy.js
@@ -5,7 +5,7 @@ const { join, resolve } = require("path");
 const { execSync } = require('child_process');
 const semver = require('semver');
 const login = require('facebook-chat-api')
-
+const keep_alive = require('./keep_alive.js')
 
 const client = new Object({
 	commands: new Map(),
